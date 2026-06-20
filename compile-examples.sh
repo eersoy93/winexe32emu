c863d682156d90c2d9b79b41def9c2587085fe1d
Erdem Ersoy <erdemersoy256@gmail.com>
2026-06-20 19:05:51 +0300

    Update copyright years

--- compile-examples.sh
+++ compile-examples.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
-# Copyright 2025 Erdem Ersoy (eersoy93)
+# Copyright 2025-2026 Erdem Ersoy (eersoy93)
 #
 # Licensed under the Apache License, Version 2.0 (the "License");
 # you may not use this file except in compliance with the License.
